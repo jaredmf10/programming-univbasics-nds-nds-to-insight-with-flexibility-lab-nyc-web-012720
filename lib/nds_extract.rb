50812ae1f141a24e4228894fe58eedd3ce0c702c
Jared Fleming <jaredmf10@gmail.com>
2020-01-22 17:23:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -72,7 +72,7 @@ def gross_per_studio(collection)
   while i < collection.length do
     movie = collection[i]
 
-    if !resule[movie[:studio]]
+    if !result[movie[:studio]]
       result[movie[:studio]] = movie[:worldwide_gross]
     else
       result[movie[:studio]] += movie[:worldwide_gross]
