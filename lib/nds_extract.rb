38c06798f20bd16d617999bf58d0c73e191fa148
Jared Fleming <jaredmf10@gmail.com>
2020-01-22 17:13:56 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
-
+require 'pp'
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
 
@@ -21,7 +21,7 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  { 
+  {
     :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
@@ -29,7 +29,7 @@ def movie_with_director_name(director_name, movie_data)
     :director_name => director_name
   }
 end
-
+movie_with_director_name
 
 # Your code after this point
 
@@ -38,16 +38,21 @@ def movies_with_director_key(name, movies_collection)
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
   # returned by this method.
-  #
+  #pp movies_collection
+  #pp name
   # INPUT:
   # * name: A director's name
   # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
+  #AoH_updated_movies = []
+  #i = 0
   # RETURN:
-  #
   # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  # :director_name key. This addition can be done by using the provided movie_with_director_name method
+  # while i < movies_collection.length do
+  #   dir_name = name
+  #
+  # end
+ #AoH_updated_movies
 end
 
 
